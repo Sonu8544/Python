21ab278ecd058c8436e7eb1c4bb313f926e73dad
Sonu8544 <sksonu8544@gmail.com>
2025-11-23 21:08:00 +0530

    dev python function and use it diffrent file

--- 01_Basic/hello_python.py
+++ 01_Basic/hello_python.py
@@ -1,1 +1,8 @@
+# Hello Python program
 print("Hello Python!")
+
+def addTwoNumbers (a, b):
+    return a + b
+
+def multiplyTwoNumbers (a, b):
+    return a * b
---
+++ 01_Basic/python_function.py
@@ -0,0 +1,9 @@
+# import add two number function python file
+from hello_python import addTwoNumbers
+result = addTwoNumbers(5, 7)
+print("Sum of 5 and 7 is:", result)
+
+# import multiply two number function python file
+from hello_python import multiplyTwoNumbers
+result = multiplyTwoNumbers(5, 7)
+print("Multiplication of 5 and 7 is:", result)
